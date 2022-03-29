b2a1918803f19f8a479e6ff439b62b0e10a188ba
Lali98 <kisslajosbertalan77@gmail.com>
2022-03-29 08:12:57 +0200

    A hex érték javitva.

--- rgb.js
+++ rgb.js
@@ -9,8 +9,12 @@ function hatter(){
 
     document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 
+    let mod_r = "0" + r.toString(16);
+    let mod_g = "0" + g.toString(16);
+    let mod_b = "0" + b.toString(16);
+
     if(document.getElementById("hex").checked){
-        document.getElementById("text").value =`#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+        document.getElementById("text").value = `#${mod_r.slice(mod_r.length - 2)}${mod_g.slice(mod_g.length - 2)}${mod_b.slice(mod_b.length  - 2)}`;
     }
     else{
         document.getElementById("text").value = `rgb(${r},${g},${b})`;
